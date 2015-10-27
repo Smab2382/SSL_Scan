5db546b4a08df1ea0d58f3614083b0977817184a
iNomaD <royalruby@yandex.ru>
2015-10-28 02:45:46 +0300

    Fixed url to host transformation

--- sslscan/heartbleed.py
+++ sslscan/heartbleed.py
@@ -6,13 +6,28 @@ import binascii
 import time
 import select
 import struct
+import urllib.parse
 
 #binary string from hex string
 def hex2bin(x):
     return binascii.unhexlify(x.replace(' ', '').replace('\n', ''))
 
 #packet with hello message we want to send
-helloPacket = hex2bin('4f 3b')
+helloPacket = hex2bin(
+'16 03 02 00 31'    # Content type = 16 (handshake message); Version = 03 02; Packet length = 00 31
+'01 00 00 2d'       # Message type = 01 (client hello); Length = 00 00 2d
+
+'03 02'             # Client version = 03 02 (TLS 1.1)
+
+# Random (uint32 time followed by 28 random bytes):
+'50 0b af bb b7 5a b8 3e f0 ab 9a e3 f3 9c 63 15 33 41 37 ac fd 6c 18 1a 24 60 dc 49 67 c2 fd 96'
+'00'                # Session id = 00
+'00 04 '            # Cipher suite length
+'00 33 c0 11'       # 4 cipher suites
+'01'                # Compression methods length
+'00'                # Compression method 0: no compression = 0
+'00 00'             # Extensions length = 0
+)
 
 def recvall(s, length, timeout=5):
     endtime = time.time() + timeout
@@ -60,18 +75,19 @@ def getTLSMessage(s, fragments=1):
 
     return contentType, version, payload
 
-def check(host, port=443):
+def check(url, port=443):
+    host = urllib.parse.urlparse(url).netloc
     print("Start scan: {0} at port {1}".format(host, port))
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     print('Connecting...')
-    sys.stdout.flush()
     s.connect((host, port))
+
     print('Sending Client Hello...')
-    sys.stdout.flush()
     s.send(helloPacket)
+
     print('Waiting for Server Hello...')
-    sys.stdout.flush()
 
     # Receive packets until we get a hello done packet
     while True:
@@ -84,7 +100,6 @@ def check(host, port=443):
             break
 
     print('Sending heartbeat request...')
-    sys.stdout.flush()
 
 def main(): #for test
 	check("fitnessland.spb.ru")
