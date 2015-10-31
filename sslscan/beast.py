8fc153160bfdfd0eef763063598f53883ec9288d
apofis <smab90@mail.ru>
2015-10-31 13:17:34 +0300

    something about ciphers

--- sslscan/beast.py
+++ sslscan/beast.py
@@ -3,14 +3,26 @@ __author__ = 'Smab'
 import socket
 import ssl
 
-host = 'mts.ru'
+shif1='AES128-SHA'
+shif2='CBC'
+host = 'support.bfb-ev.de'
 port = 443
 sert=ssl.get_server_certificate((host, port),ssl.PROTOCOL_TLSv1,None)
 sock = socket.socket()
 sslsock=ssl.SSLSocket(sock)
 sslsock.connect((host, port))
 var=sslsock.cipher()
-print(var[0])
+b=True
+if var[0].find(shif2,0,len(var[0]))>=0:
+    print('vulnerability server-side')
+    b=False
+else:
+    if var[0].find(shif1,0,len(var[0]))>=0:
+        print('mitigated server-side')
+        b=False
+if b:
+    print('no mitigated server-side')
+print(var)
 
 
 
