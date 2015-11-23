88fde90c0e0fb560c8b8407427d6435c67a41369
hulponot <hulponot@gmail.com>
2015-11-23 20:22:00 +0300

    several versions connections try, time parse

--- sslscan/sertificat.py
+++ sslscan/sertificat.py
@@ -1,18 +1,48 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 __author__ = 'hulponot'
 
-import socket, ssl, pprint
+import socket, ssl, pprint, re
 
 def cert_info(hostname):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ssl_sock = ssl.wrap_socket(sock);
-    ssl_sock.connect((hostname, 443))
-    pprint.pprint(ssl_sock.getpeercert(0))
-    pprint.pprint(ssl_sock.cipher())
+    
+    protocols = (ssl.PROTOCOL_SSLv2, ssl.PROTOCOL_SSLv3, ssl.PROTOCOL_SSLv23, ssl.PROTOCOL_TLSv1, ssl.PROTOCOL_TLSv1_1, ssl.PROTOCOL_TLSv1_2)
 
-    pprint.pprint(ssl_sock.version())
+    for p in protocols:
+        context = ssl.SSLContext(p)
+        context.load_default_certs()
+        context.verify_mode = ssl.CERT_REQUIRED
+        conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=hostname)
+        try:
+            conn.connect((hostname,443))
+            cert = conn.getpeercert()
+            #pprint.pprint (cert)
+        except ConnectionResetError:
+            print ("connection refused with ",resolve_to_protocol_string(p))
+        except ssl.SSLError:
+            print ("handshake or so refused with ",resolve_to_protocol_string(p))
+    dateBefore = cert["notBefore"]
+    date = re.split(' +', dateBefore)
+    monthB = date[0]
+    yearB = date[3]
+    dayB = date[1]
+    (hourB,minuteB,secB) = re.split(':', date[2])
+    print (hourB)
+    dateAfter = cert["notAfter"]
 
-    ssl_sock.close()
-    sock.close()
 
-cert_info('vk.com')
+def resolve_to_protocol_string(protocol_int):
+    if (protocol_int == 0):
+        return ("PROTOCOL_SSLv2")
+    if (protocol_int == 1):
+        return "PROTOCOL_SSLv3"
+    if (protocol_int == 2):
+        return "PROTOCOL_SSLv23"
+    if (protocol_int == 3):
+        return "PROTOCOL_TSLv1"
+    if (protocol_int == 4):
+        return "PROTOCOL_TSLv1.1"
+    if (protocol_int == 5):
+        return "PROTOCOL_TSLv1.2"
+
+
+cert_info('www.python.org')
