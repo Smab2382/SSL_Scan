72ded0dcfa59167eaef999945d784c3eb3c57490
Smab2382 <smab90@mail.ru>
2015-12-02 02:39:17 +0300

    logjam(proto)

--- sslscan/logjam.py
+++ sslscan/logjam.py
@@ -2,32 +2,30 @@ __author__ = 'Smab'
 
 import socket
 import ssl
-def funbest(host):
-    shif2='CBC'
+
+def funlogjam(host):
+    shif2='DH'
     port = 443
     sock = socket.socket()
     sslsock=ssl.SSLSocket(sock)
-    context = sslsock.context
-    context.options=ssl.OP_NO_TLSv1_2
-    context.options=ssl.OP_NO_TLSv1_1
+    context=sslsock.context
     try:
         sslsock.connect((host, port))
     except ConnectionError:
         print("connect error")
     except ConnectionResetError:
         print("connect error")
-    except ssl.SSLError:
-        print("no vulnerability server-side")
     var=sslsock.cipher()
     if var!=None:
         if var[0].find(shif2,0,len(var[0]))>=0:
-            print('vulnerability server-side')
-        else:
-            print('no vulnerability server-side')
+            b=True
+            try:
+                sslsock.do_handshake((host, port))
+            except ssl.SSLError:
+                print("no vulnerability server-side")
+                b=False
+            if b: print("vulnerability server-side")
     print(var)
     sslsock.close()
 
-host = 'google.ru'
-funbest(host)
-
-
+funlogjam("yandex.ru")
