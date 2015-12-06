78be1e32aabbbec6eea4ab393e0c310de6fdc995
kowalskiAG <kowalskiD@bk.ru>
2015-12-07 00:05:14 +0300

    Poodle

--- sslscan/poodle.py
+++ sslscan/poodle.py
@@ -1,19 +1,17 @@
-__author__ = 'Elena'
+__author__ = 'kowalskiAG'
 
-try:
-import ssl
-except ImportError:
-pass
-Else:
-import socket, ssl, pprint
+import socket, ssl
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-ssl_sock = ssl.wrap_socket(s, ca_certs="/etc/ca_certs_file", cert_reqs=ssl.CERT_REQUIRED)
-ssl_sock.connect(('www.google.com', 443))
-
-pprint.pprint(ssl_sock.getpeercert())
-
+ssl_sock=ssl.SSLSocket(s)
+context = ssl_sock.context
+context.options=ssl.OP_NO_TLSv1_2
+context.options=ssl.OP_NO_TLSv1_1
+context.options=ssl.OP_NO_TLSv1
+try:
+    ssl_sock.connect(('www.google.com', 443))
+except ConnectionResetError:
+    print("fu")
+    print("no")
+print(ssl_sock.version())
 ssl_sock.close()
-context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-context.verify_mode = ssl.CERT_REQUIRED
