d617df2fb47f6c6b66ae3c6d133e129fb6c91f32
kowalskiAG <kowalskiD@bk.ru>
2015-12-12 17:51:47 +0300

    Poodle function for interface

--- sslscan/poodle.py
+++ sslscan/poodle.py
@@ -1,17 +1,32 @@
 __author__ = 'kowalskiAG'
 
 import socket, ssl
+from sslscan import status
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ssl_sock=ssl.SSLSocket(s)
-context = ssl_sock.context
-context.options=ssl.OP_NO_TLSv1_2
-context.options=ssl.OP_NO_TLSv1_1
-context.options=ssl.OP_NO_TLSv1
-try:
-    ssl_sock.connect(('www.google.com', 443))
-except ConnectionResetError:
-    print("fu")
-    print("no")
-print(ssl_sock.version())
-ssl_sock.close()
+
+def poodlefun(host):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ssl_sock=ssl.SSLSocket(s)
+    context = ssl_sock.context
+    context.options=ssl.OP_NO_TLSv1
+    try:
+        ssl_sock.connect((host, 443))
+    except ConnectionError:
+        print("connect error")
+        return status.Status.stError
+    except ConnectionResetError:
+        print("connect error")
+        return status.Status.stError
+    except ssl.SSLError:
+        print("NO VULNERABLE")
+        return status.Status.stOk
+    else:
+        print("VULNERABLE")
+        return status.Status.stVuln
+
+def main():
+    host = 'www.google.com'
+    poodlefun(host)
+
+if __name__ == '__main__':
+    main()
